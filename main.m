51fc9027849f45636fd3b4d5c27bc8ba066a4552
Francisco Igor <franciscoigor@gmail.com>
2018-08-14 17:35:24 -0700

    initial commit

---
+++ main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  ObjectiveC-Example
+//
+//  Created by Francisco on 2018-08-13.
+//  Copyright © 2018 Francisco. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+int main(int argc, const char * argv[]) {
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+    
+
+    [pool drain];
+    return 0;
+}
---
+++ make.sh
@@ -0,0 +1,1 @@
+gcc `gnustep-config --objc-flags` -lobjc -lgnustep-base main.m -o main
